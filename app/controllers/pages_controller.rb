45ae5e31accae19eb1db2952792a737750ec753a
Rabea Bader <bdr193@gmail.com>
2020-05-10 09:55:25 +0300

    finished with params

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,16 @@
 class PagesController < ApplicationController
   def about
-    
+
+  end
+
+  def homepage
+
+  end
+
+  def contact
+    @members = ["Sam", "Bob", "Adam", "Rob"]
+    if params[:member]
+      @members= @members.select {|member| member.starts_with?(params[:member].capitalize) }
+    end
   end
 end
